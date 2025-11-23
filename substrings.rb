fd2859495919a5547c5ccaa4a0b4d8c2099f1958
gbntb <228612152+gbntb@users.noreply.github.com>
2025-11-23 08:41:03 -0400

    Ignore the case when searching for the substring

--- substrings.rb
+++ substrings.rb
@@ -2,10 +2,11 @@ def substrings(string, dictionary)
   dictionary.inject(Hash.new(0)) do |result, substring|
     count = 0
     partial_string = string.downcase
+    dict_string = substring.downcase
     
-    while partial_string.index substring do
+    while partial_string.index dict_string do
       count += 1
-      start_index = partial_string.index(substring) + substring.length
+      start_index = partial_string.index(dict_string) + dict_string.length
       partial_string = partial_string[start_index..-1]
     end
 
